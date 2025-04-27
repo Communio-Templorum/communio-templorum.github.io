e894969c81bf75ae63855e1ef51069aaa08cab77
Sam Grundman <samgrundman27@gmail.com>
2025-04-27 17:44:55 -0400

    Update toolkit and dependencies

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,1 @@
-"use strict";window.json={},yodasws.page("home").setRoute({template:"pages/home.html",route:"/"});
+"use strict";yodasws.page("home").setRoute({template:"pages/home.html",route:"/"});
